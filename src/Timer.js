9d7e9fad29d2a7414b840e355d7667819240bc60
Nisha Singhal <nishasinghal979@gmail.com>
2023-04-27 09:04:40 +0530

    Added timer component to set duration

---
+++ src/Timer.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+function Timer(props) {
+    const [studyTime, setStudyTime] = useState(0);
+    const [breakTime, setBreakTime] = useState(0);
+
+    const handleStudyTimeChange = (event) => {
+        setStudyTime(event.target.value);
+    };
+
+    const handleBreakTimeChange = (event) => {
+        setBreakTime(event.target.value);
+    };
+
+    return (
+        <div>
+            <h2>Timer</h2>
+            <p>Hello, {props.name}! Please set the study time and break time:</p>
+            <div>
+                <label>Study time:</label>
+                <input type="number" value={studyTime} onChange={handleStudyTimeChange} />
+            </div>
+            <div>
+                <label>Break time:</label>
+                <input type="number" value={breakTime} onChange={handleBreakTimeChange} />
+            </div>
+        </div>
+    );
+}
+
+export default Timer;
