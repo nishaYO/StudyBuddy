6716cc56c911cb6c1cf0742eb2fdf5a3b8faa8d4
Nisha Singhal <nishasinghal979@gmail.com>
2023-05-11 08:17:20 +0530

    built setmusic component

--- src/SetMusic.js
+++ src/SetMusic.js
@@ -1,30 +1,60 @@
-import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import React from 'react';
+import { useState } from 'react';
+import './stylesheets/SetMusic.css';
 
-
-const focusMusicList = [
+const soundList = [
     {
         id: 1,
         name: "Meditation",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://media.giphy.com/media/3o8dFlRx9QGDdfs3UQ/giphy.gif",
+        audio: './sounds-and-effects/sounds/purrple-cat-crescent-moon.mp3',
+        volume: 0.5
     },
     {
         id: 2,
         name: "Nature Sounds",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://media.giphy.com/media/n9h61thJkq6Xe/giphy.gif",
+        audio: './sounds-and-effects/sounds/Ghostrifter-Official-Celestia.mp3',
+        volume: 0.5
     },
     {
         id: 3,
         name: "Calm Music",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://media.giphy.com/media/q10hztkXiFFmhBbH2l/giphy.gif",
+        audio: './sounds-and-effects/sounds/Lost-and-Found.mp3',
+        volume: 0.5
+    },
+];
+
+const effectList = [
+    {
+        id: 1,
+        name: "Rain",
+        image: "https://media.giphy.com/media/1fnu914Z79qQpVi2xZ/giphy.gif",
+        audio: "soundsAndEffects\effects\effect_rain.mp3",
+        volume: 0.5
+    },
+    {
+        id: 2,
+        name: "Fire",
+        image: "https://media.giphy.com/media/rkSu72ptAZseQ/giphy.gif",
+        audio: "soundsAndEffects\effects\effect_fire.mp3",
+        volume: 0.5
+    },
+    {
+        id: 3,
+        name: "Forest",
+        image: "https://media.giphy.com/media/VFqafDSTxP0ic/giphy.gif",
+        audio: "soundsAndEffects\effects\effect_forest.mp3",
+        volume: 0.5
     },
 ];
 
 const SetMusic = () => {
-
     const navigate = useNavigate();
     const [selectedMusic, setSelectedMusic] = useState(null);
+    const [isMuted, setIsMuted] = useState(false);
 
     const handleMusicSelect = (music) => {
         setSelectedMusic(music);
@@ -38,33 +68,53 @@ const SetMusic = () => {
         navigate('/session-started');
     };
 
+    const handleVolumeClick = () => {
+        setIsMuted(!isMuted);
+    }
 
     return (
-        <div>
-            <h2>Select Focus Music</h2>
-            <div>
-                {focusMusicList.map((music) => (
-                    <div key={music.id} onClick={() => handleMusicSelect(music)}>
-                        <img src={music.image} alt={music.name} />
-                        <p>{music.name}</p>
+        <div id="music-container">
+            <h1>Select Focus Music</h1>
+            <div id="music-form">
+                <div className="sounds flex-container-column">
+                    <h2>Sounds</h2>
+                    <div className="flex-container-row">
+                        {soundList.map((music) => (
+                            <div className="sound flex-container-column" key={music.id} onClick={() => handleMusicSelect(music)}>
+                                <img width="150px" height="150px" src={music.image} alt={music.name} />
+                                <p>{music.name}</p>
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
-            {selectedMusic && (
-                <div>
-                    <h3>You have selected:</h3>
-                    <img src={selectedMusic.image} alt={selectedMusic.name} />
-                    <p>{selectedMusic.name}</p>
                 </div>
-            )}
-            <button onClick={handlePrevClick}>Previous</button>
-            <button onClick={handleNextClick}>Next</button>
+                <div className="effects flex-container-column">
+                    <h2>Effects</h2>
+                    <div className="flex-container-row">
+                        {effectList.map((music) => (
+                            <div className="effect flex-container-column" key={music.id} onClick={() => handleMusicSelect(music)}>
+                                <img width="150px" height="150px" src={music.image} alt={music.name} />
+                                <p>{music.name}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            {/* {selectedMusic && (
+                <div>
+                    <h2>Selected Music</h2>
+                    <audio src={selectedMusic.audio} autoPlay loop volume={selectedMusic.volume} />
+                </div>
+            )} */}
+            <button id="prevbtn" className="navigation-button" onClick={handlePrevClick}>Previous</button>
+            <button id="nextbtn" className="navigation-button" onClick={handleNextClick}>Next</button>
+            {/* <button id="volumebtn" className="navigation-button" onClick={handleVolumeClick}>
+                {isMuted ? <i className="fas fa-volume-mute"></i> : <i className="fas fa-volume-up"></i>}
+            </button> */}
         </div>
     );
 };
 
 export default SetMusic;
-
 
 
 
