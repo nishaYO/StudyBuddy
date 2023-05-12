bff67d5647c0c099474c2550e20a870df488226b
Nisha Singhal <nishasinghal979@gmail.com>
2023-05-12 07:01:47 +0530

    Improve setmusic component

--- src/SetMusic.js
+++ src/SetMusic.js
@@ -2,27 +2,31 @@ import { useNavigate } from 'react-router-dom';
 import React from 'react';
 import { useState } from 'react';
 import './stylesheets/SetMusic.css';
+import { FaVolumeUp } from 'react-icons/fa';
+import { FaVolumeDown } from 'react-icons/fa';
+import { FaVolumeMute } from 'react-icons/fa';
+import { FaVolumeOff } from 'react-icons/fa';
 
 const soundList = [
     {
         id: 1,
         name: "Meditation",
         image: "https://media.giphy.com/media/3o8dFlRx9QGDdfs3UQ/giphy.gif",
-        audio: './sounds-and-effects/sounds/purrple-cat-crescent-moon.mp3',
+        audio: require('./soundsAndEffects/sounds/purrple-cat-crescent-moon.mp3'),
         volume: 0.5
     },
     {
         id: 2,
         name: "Nature Sounds",
         image: "https://media.giphy.com/media/n9h61thJkq6Xe/giphy.gif",
-        audio: './sounds-and-effects/sounds/Ghostrifter-Official-Celestia.mp3',
+        audio: require('./soundsAndEffects/sounds/Ghostrifter-Official-Celestia.mp3'),
         volume: 0.5
     },
     {
         id: 3,
         name: "Calm Music",
         image: "https://media.giphy.com/media/q10hztkXiFFmhBbH2l/giphy.gif",
-        audio: './sounds-and-effects/sounds/Lost-and-Found.mp3',
+        audio: require('./soundsAndEffects/sounds/Lost-and-Found.mp3'),
         volume: 0.5
     },
 ];
@@ -32,21 +36,21 @@ const effectList = [
         id: 1,
         name: "Rain",
         image: "https://media.giphy.com/media/1fnu914Z79qQpVi2xZ/giphy.gif",
-        audio: "soundsAndEffects\effects\effect_rain.mp3",
+        audio: require('./soundsAndEffects/effects/effect_rain.mp3'),
         volume: 0.5
     },
     {
         id: 2,
         name: "Fire",
         image: "https://media.giphy.com/media/rkSu72ptAZseQ/giphy.gif",
-        audio: "soundsAndEffects\effects\effect_fire.mp3",
+        audio: require('./soundsAndEffects/effects/effect_fire.mp3'),
         volume: 0.5
     },
     {
         id: 3,
         name: "Forest",
         image: "https://media.giphy.com/media/VFqafDSTxP0ic/giphy.gif",
-        audio: "soundsAndEffects\effects\effect_forest.mp3",
+        audio: require('./soundsAndEffects/effects/effect_forest.mp3'),
         volume: 0.5
     },
 ];
@@ -75,9 +79,16 @@ const SetMusic = () => {
     return (
         <div id="music-container">
             <h1>Select Focus Music</h1>
+            {/* <FaVolumeUp size={20} />
+            <FaVolumeOff size={20} />
+            <FaVolumeMute size={20} />
+            <FaVolumeDown size={20} /> */}
             <div id="music-form">
                 <div className="sounds flex-container-column">
-                    <h2>Sounds</h2>
+                    <header className='flex-container-row'>
+                        <h2>Sounds</h2>
+                        <FaVolumeUp size={20} />
+                    </header>
                     <div className="flex-container-row">
                         {soundList.map((music) => (
                             <div className="sound flex-container-column" key={music.id} onClick={() => handleMusicSelect(music)}>
@@ -88,7 +99,11 @@ const SetMusic = () => {
                     </div>
                 </div>
                 <div className="effects flex-container-column">
-                    <h2>Effects</h2>
+                    <header className='flex-container-row'>
+                        <h2>Effects</h2>
+                        <FaVolumeUp size={20} />
+                    </header>
+
                     <div className="flex-container-row">
                         {effectList.map((music) => (
                             <div className="effect flex-container-column" key={music.id} onClick={() => handleMusicSelect(music)}>
@@ -99,17 +114,16 @@ const SetMusic = () => {
                     </div>
                 </div>
             </div>
-            {/* {selectedMusic && (
+            {selectedMusic && (
                 <div>
-                    <h2>Selected Music</h2>
-                    <audio src={selectedMusic.audio} autoPlay loop volume={selectedMusic.volume} />
+                    <h2>Selected Music {selectedMusic.name}</h2>
+                    <audio src={selectedMusic.audio.default} autoPlay loop volume={selectedMusic.volume} mute={false} />
                 </div>
-            )} */}
+            )}
+
             <button id="prevbtn" className="navigation-button" onClick={handlePrevClick}>Previous</button>
             <button id="nextbtn" className="navigation-button" onClick={handleNextClick}>Next</button>
-            {/* <button id="volumebtn" className="navigation-button" onClick={handleVolumeClick}>
-                {isMuted ? <i className="fas fa-volume-mute"></i> : <i className="fas fa-volume-up"></i>}
-            </button> */}
+
         </div>
     );
 };
