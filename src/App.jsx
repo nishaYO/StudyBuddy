3ed65c8603306016b84ee29d5237fc37d9298219
adarshswamianth <adarshswaminath7@gmail.com>
2023-11-09 11:31:08 +0530

    feat: welcome page and localstorage functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import StarterPage from "./Components/StarterPage";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [getName, setGetname] = useState("");
+  // function to get the username in localstorage
+  const handleSubmit = () => {
+    localStorage.setItem("username", username);
+    window.location.reload();
+  };
+  // fetch username from localstorage
+  useEffect(() => {
+    setGetname(localStorage.getItem("username"));
+  }, []);
+
   return (
-    <div>Its Working</div>
-  )
+    <div className="font-mono bg-[#FFF3DA] p-2 min-h-screen ">
+      {getName ? (
+        <h1 className="flex items-center justify-center">Hello {getName}</h1>
+      ) : (
+        <StarterPage setUsername={setUsername} handleSubmit={handleSubmit} />
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
