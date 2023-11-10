9b94c4533dea430a554bac9025a873d4d00a54b4
Nisha Singhal <nishasinghal979@gmail.com>
2023-11-10 17:32:45 +0530

    SetMusic built

--- src/Components/SessionSetup/SetMusic.jsx
+++ src/Components/SessionSetup/SetMusic.jsx
@@ -1,3 +1,51 @@
+// const soundList = [
+//   {
+//     id: 1,
+//     name: "Meditation",
+//     image: "https://media.giphy.com/media/3o8dFlRx9QGDdfs3UQ/giphy.gif",
+//     audio: require("./soundsAndEffects/sounds/purrple-cat-crescent-moon.mp3"),
+//     volume: 0.5,
+//   },
+//   {
+//     id: 2,
+//     name: "Nature Sounds",
+//     image: "https://media.giphy.com/media/n9h61thJkq6Xe/giphy.gif",
+//     audio: require("./soundsAndEffects/sounds/Ghostrifter-Official-Celestia.mp3"),
+//     volume: 0.5,
+//   },
+//   {
+//     id: 3,
+//     name: "Calm Music",
+//     image: "https://media.giphy.com/media/q10hztkXiFFmhBbH2l/giphy.gif",
+//     audio: require("./soundsAndEffects/sounds/Lost-and-Found.mp3"),
+//     volume: 0.5,
+//   },
+// ];
+
+// const effectList = [
+//   {
+//     id: 1,
+//     name: "Rain",
+//     image: "https://media.giphy.com/media/1fnu914Z79qQpVi2xZ/giphy.gif",
+//     audio: require("./soundsAndEffects/effects/effect_rain.mp3"),
+//     volume: 0.5,
+//   },
+//   {
+//     id: 2,
+//     name: "Fire",
+//     image: "https://media.giphy.com/media/rkSu72ptAZseQ/giphy.gif",
+//     audio: require("./soundsAndEffects/effects/effect_fire.mp3"),
+//     volume: 0.5,
+//   },
+//   {
+//     id: 3,
+//     name: "Forest",
+//     image: "https://media.giphy.com/media/VFqafDSTxP0ic/giphy.gif",
+//     audio: require("./soundsAndEffects/effects/effect_forest.mp3"),
+//     volume: 0.5,
+//   },
+// ];
+
 function SetMusic() {
  
     return (
