2e277b1fae3f248698fc22bdf415ad23d3ee14b8
Nisha Singhal <nishasinghal979@gmail.com>
2023-11-15 13:45:38 +0530

    Built setBreaks UI

--- src/Components/Help.jsx
+++ src/Components/Help.jsx
@@ -1,12 +1,118 @@
+import React, { useState } from "react";
+// require('dotenv').config();// Load environment variables from .env file
+// const backend_endpoint = process.env.SERVER_ENDPOINT || 'http://localhost:5000/contact/submit';
+const backend_endpoint = 'http://localhost:5000/contact/submit';
+
 function Help() {
- 
-    return (
-      <div>
-        hello i am nice Help function
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(backend_endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        console.log('Contact form submitted successfully!');
+        // You can handle success here (e.g., show a success message)
+      } else {
+        console.error('Failed to submit contact form');
+        // You can handle errors here (e.g., show an error message)
+      }
+    } catch (error) {
+      console.error('Error submitting contact form:', error);
+      // You can handle errors here (e.g., show an error message)
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center font-mono">Help Page</h1>
+
+      <div className="container mx-auto mt-8 p-8 w-3/4 bg-[#BEADFA] border border-[#BEADFA] rounded">
+        <h1 className="text-4xl mb-6">Contact Us</h1>
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <div className="flex flex-col">
+            <label htmlFor="name" className="text-sm font-semibold">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="border rounded p-2 text-sm"
+              placeholder="Your Name"
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label htmlFor="email" className="text-sm font-semibold">
+              Email Address
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="border rounded p-2 text-sm"
+              placeholder="Your Email Address"
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label htmlFor="message" className="text-sm font-semibold">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              className="border rounded p-2 text-sm"
+              placeholder="Your Message"
+              rows="4"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="p-2 bg-orange-300 text-white rounded text-sm"
+          >
+            Submit
+          </button>
+        </form>
       </div>
-    );
-  }
-  
-  
-  
-  export default Help;
+      <div className="p-4 bg-gray-100 border border-[#BEADFA] rounded m-4 w-3/4 container mx-auto ">
+        <p className="text-sm text-gray-700">
+          Study Buddy is your study companion designed for efficiency. It
+          simplifies your study routine, offering timer and music settings,
+          ensuring a seamless learning experience. Whether setting up your first
+          session or tracking your progress, Study Buddy makes studying
+          straightforward and productive.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Help;
--- src/Components/SessionSetup/SetBreaks.jsx
+++ src/Components/SessionSetup/SetBreaks.jsx
@@ -1,12 +1,140 @@
-function SetBreaks() {
- 
-    return (
-      <div>
-        hello i am nice SetBreaks function
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt, faPlus } from "@fortawesome/free-solid-svg-icons";
+
+const SetBreaks = ({ totalDuration }) => {
+  const [breaks, setBreaks] = useState([
+    {
+      breakDuration: { hours: "", minutes: "", seconds: "" },
+      studyDuration: { hours: "", minutes: "", seconds: "" },
+    },
+  ]);
+  
+
+  const addBreak = () => {
+    setBreaks([
+      ...breaks,
+      {
+        breakDuration: { hours: "", minutes: "", seconds: "" },
+        studyDuration: { hours: "", minutes: "", seconds: "" },
+      },
+    ]);
+  };
+
+  const removeBreak = (index) => {
+    const newBreaks = [...breaks];
+    newBreaks.splice(index, 1);
+    setBreaks(newBreaks);
+  };
+
+  const handleBreakChange = (event, index, field) => {
+    const newBreaks = [...breaks];
+    newBreaks[index][field] = event.target.value;
+    setBreaks(newBreaks);
+  };
+
+  return (
+    <div className="container mx-auto mt-8 p-8 bg-gray-100 ">
+      <h1 className="text-4xl font-bold mb-6">Set The Breaks</h1>
+      <div className="space-y-4 font-mono  bg-[#FFF3DA] ">
+        {breaks.map((breakItem, index) => (
+          <div key={index} className="space-y-2 border-2 rounded-lg border-[#BEADFA]">
+            <div className="flex items-center space-x-2">
+              <div className="flex items-center space-x-2">
+                <input
+                  type="number"
+                  placeholder="Hours"
+                  value={breakItem.breakDuration.hours}
+                  onChange={(event) =>
+                    handleBreakChange(event, index, "breakDuration.hours")
+                  }
+                  className="border rounded p-2 text-sm w-1/4"
+                />
+                <span>hours</span>
+              </div>
+              <div className="flex items-center space-x-2">
+                <input
+                  type="number"
+                  placeholder="Minutes"
+                  value={breakItem.breakDuration.minutes}
+                  onChange={(event) =>
+                    handleBreakChange(event, index, "breakDuration.minutes")
+                  }
+                  className="border rounded p-2 text-sm w-1/4"
+                />
+                <span>minutes</span>
+              </div>
+              <div className="flex items-center space-x-2">
+                <input
+                  type="number"
+                  placeholder="Seconds"
+                  value={breakItem.breakDuration.seconds}
+                  onChange={(event) =>
+                    handleBreakChange(event, index, "breakDuration.seconds")
+                  }
+                  className="border rounded p-2 text-sm w-1/4"
+                />
+                <span>seconds</span>
+              </div>
+            </div>
+            <div className="flex items-center space-x-2">
+              <div className="flex items-center space-x-2">
+                <input
+                  type="number"
+                  placeholder="Hours"
+                  value={breakItem.studyDuration.hours}
+                  onChange={(event) =>
+                    handleBreakChange(event, index, "studyDuration.hours")
+                  }
+                  className="border rounded p-2 text-sm w-1/4"
+                />
+                <span>hours</span>
+              </div>
+              <div className="flex items-center space-x-2">
+                <input
+                  type="number"
+                  placeholder="Minutes"
+                  value={breakItem.studyDuration.minutes}
+                  onChange={(event) =>
+                    handleBreakChange(event, index, "studyDuration.minutes")
+                  }
+                  className="border rounded p-2 text-sm w-1/4"
+                />
+                <span>minutes</span>
+              </div>
+              <div className="flex items-center space-x-2">
+                <input
+                  type="number"
+                  placeholder="Seconds"
+                  value={breakItem.studyDuration.seconds}
+                  onChange={(event) =>
+                    handleBreakChange(event, index, "studyDuration.seconds")
+                  }
+                  className="border rounded p-2 text-sm w-1/4"
+                />
+                <span>seconds</span>
+              </div>
+              <button
+                onClick={() => removeBreak(index)}
+                className="p-2 bg-red-500 text-white rounded text-sm"
+              >
+                <FontAwesomeIcon icon={faTrashAlt} />
+              </button>
+            </div>
+          </div>
+        ))}
+        <button
+          onClick={addBreak}
+          className="p-2 bg-green-500 text-white rounded text-sm"
+        >
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
   
   
   
-  export default SetBreaks;
+};
+
+export default SetBreaks;
