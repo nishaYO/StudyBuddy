56eb439487010289a0127e1bb91cd56c04b8ba59
Nisha Singhal <nishasinghal979@gmail.com>
2023-11-17 23:15:48 +0530

    Reports fetched from backend

--- src/Components/Reports.jsx
+++ src/Components/Reports.jsx
@@ -1,19 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'wouter';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "wouter";
 
 function Reports() {
   const [location, navigate] = useLocation();
-
-  // sample reports object
-  // to be replaced by the object received from the backend when user navigates to this page.
-  const reports = {
-    "current Streak": 0,
-    "Highest Streak": 0,
-    "Highest Hours Session": 0,
-    "total Hours": 0,
-    "today total Hours": 0
-  }
   
+  // sample reports object
+  const sampleReports = {
+    "main-stats": {
+      "current Streak": 0,
+      "Highest Streak": 0,
+      "Highest Hours local Session": 0,
+      "total Hours": 0,
+      "today total Hours": 0,
+    },
+  };
+  const [reports, setReports] = useState(sampleReports);
+
+  useEffect(() => {
+    const fetchReports = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/reports/fetch");
+        if (response.ok) {
+          const data = await response.json();
+          setReports(data.reports);
+        }
+      } catch (error) {
+        console.error("Error fetching reports:", error);
+      }
+    };
+
+    fetchReports();
+  }, []);
 
   const handlePreviousClick = () => {
     // todo: Navigate back to the previous page
@@ -21,18 +38,25 @@ function Reports() {
   };
 
   return (
-    <div className='m-5 flex flex-col items-center'>
+    <div className="m-5 flex flex-col items-center">
       <button
         className="bg-[#D0BFFF] text-white px-4 py-2 m-2 rounded"
         onClick={handlePreviousClick}
       >
         Back
       </button>
-        <h2 className=''>See your Reports📝</h2>
-        <div className="bg-white flex flex-col items-center w-full lg:w-1/2 p-5 m-4 border-2 rounded-lg border-[#BEADFA] shadow-lg">
-        {Object.keys(reports).map((key) => (
-          <p key={key} className='m-5'>{key.charAt(0).toUpperCase() + key.slice(1)}: {reports[key]}</p>
-        ))}
+      <h2 className="">See your Reports📝</h2>
+      <div className="bg-white flex flex-col items-center w-full lg:w-1/2 p-5 m-4 border-2 rounded-lg border-[#BEADFA] shadow-lg">
+        <div className="m-5">
+          <ul>
+            {Object.keys(reports["main-stats"]).map((key) => (
+              <li key={key} className="m-8">
+                {key.charAt(0).toUpperCase() + key.slice(1)}:{" "}
+                {reports["main-stats"][key]}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
