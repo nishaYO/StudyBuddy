3c82ce75d7cd301fcf9e7d7e24182c6668aea4dd
Nisha Singhal <nishasinghal979@gmail.com>
2023-11-17 18:01:40 +0530

    Add back button to the help page

--- src/Components/Help.jsx
+++ src/Components/Help.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Contact from './ContactForm';
-
+import { useLocation } from 'wouter';
 
 function Help() {
-
+  const [location, navigate] = useLocation();
+  const handlePreviousClick = () => {
+    // todo: Navigate back to the previous page
+    navigate("/");
+  };
   return (
     <div>
+      <button
+        className="bg-[#D0BFFF] text-white px-4 py-2 m-7 rounded"
+        onClick={handlePreviousClick}
+      >
+        Back
+      </button>
       <h1 className="text-3xl font-bold text-center font-mono">Help Page</h1>
 
       <div className="container mx-auto mt-8 p-8 w-3/4 bg-[#BEADFA] border border-[#BEADFA] rounded">
