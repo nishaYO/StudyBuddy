b0446e8aebf405907b0ec2309b03c7295673ae5c
adarshswaminath <adarshswaminath7@gmail.com>
2024-02-07 09:33:32 +0530

    style: user icon style applied

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,6 +5,7 @@ import SignupPopup from "./auth/Signup";
 import LoginPopup from "./auth/Login";
 import { useQuery } from "@apollo/client";
 import { AUTO_LOGIN_QUERY } from "../graphql/queries";
+import { FaRegUser } from "react-icons/fa";
 
 function NavbarIcons({ onNotificationsClick }) {
   return (
@@ -166,18 +167,13 @@ function Navbar() {
         {/* user auth */}
         {isRegistered ? (
           isSignedIn ? (
-            <Link href="/user">
-              <button className="text-2xl cursor-pointer">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="24"
-                  width="19"
-                  viewBox="0 0 448 512"
-                >
-                  <path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z" />
-                </svg>{" "}
+            <div className="group">
+              <Link href="/user">
+              <button className="text-2xl cursor-pointer p-2 bg-white group-hover:bg-purple-500 rounded-full border border-transparent">
+               <FaRegUser className="group-hover:text-white"/>
               </button>
             </Link>
+            </div>
           ) : (
             <button
               className="bg-[#BEADFA] px-4 py-1 border-2 border-black"
