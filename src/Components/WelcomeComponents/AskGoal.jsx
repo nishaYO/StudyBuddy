b031c62ccb0196d0a592905868daafeb127eb787
adarshswaminath <adarshswaminath7@gmail.com>
2024-03-15 05:54:48 +0530

    feature: Improve the AskGoal component design and responsive

--- src/Components/WelcomeComponents/AskGoal.jsx
+++ src/Components/WelcomeComponents/AskGoal.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { plusIcon } from "../SVG";
-
+import {FaArrowUp,FaArrowDown} from "react-icons/fa"
 function AskGoal({ handleGoalSubmit }) {
   // Retrieve streak goal from local storage on component mount
-  const storedStreakGoal = JSON.parse(localStorage.getItem("streakGoal")) || {
-    hours: "04",
-    minutes: "0",
-  };
+  const storedStreakGoal =
+    JSON.parse(localStorage.getItem("streakGoal")) || {
+      hours: "04",
+      minutes: "0",
+    };
   const [streakGoal, setStreakGoal] = useState(storedStreakGoal);
 
   // Update local storage when streak goal changes
@@ -15,14 +16,15 @@ function AskGoal({ handleGoalSubmit }) {
   };
 
   return (
-    <div className="p-2 flex flex-col items-center justify-center min-h-screen">
-      <div className="bg-white flex flex-col items-center w-full lg:w-1/2 p-6 border-2 rounded-lg border-[#BEADFA] shadow-lg">
-        <h1 className="text-3xl font-extrabold max-w-lg mb-4 text-center">
+    <div className="p-3 flex flex-col items-center justify-center min-h-screen">
+      <div className="bg-white w-full lg:w-1/2 p-6 border-2 rounded-lg border-[#BEADFA] shadow-lg">
+        <h1 className="text-3xl font-bold text-center mb-6">
           Set Your Daily Study Goal
         </h1>
-        <div className="flex flex-row gap-7 items-center m-3">
-          <div className="flex items-center">
-            {/* Add up and down arrow buttons */}
+        <div className="flex p-2 border border-gray-300 rounded-lg lg:flex-row items-center justify-center gap-4">
+          <div className="flex flex-col items-center gap-2">
+            <span className="font-bold">Hours</span>
+            <div className="flex flex-col items-center gap-2">
             <button
               onClick={() =>
                 setStreakGoal({
@@ -30,32 +32,20 @@ function AskGoal({ handleGoalSubmit }) {
                   hours: (parseInt(streakGoal.hours, 10) + 1).toString(),
                 })
               }
-              className="bg-[#D0BFFF] px-2 py-2 rounded-full mr-2"
+              className="bg-[#D0BFFF] px-2 py-2 rounded-lg"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <FaArrowUp/>
             </button>
             <input
-              type="text"
+              type="number"
               onChange={(e) =>
                 setStreakGoal({ ...streakGoal, hours: e.target.value })
               }
               value={streakGoal.hours}
-              className="w-20 border border-[#BEADFA] p-3 rounded-lg outline-none mr-2"
+              className="w-20 border border-[#BEADFA] p-3 rounded-lg text-center"
+              aria-label="Hours"
             />
-            <span>hrs</span>
-            {/* Add up and down arrow buttons */}
-            <button
+           <button
               onClick={() =>
                 setStreakGoal({
                   ...streakGoal,
@@ -65,24 +55,17 @@ function AskGoal({ handleGoalSubmit }) {
                   ).toString(),
                 })
               }
-              className="bg-[#D0BFFF] px-2 py-2 rounded-full ml-2"
+              className="bg-[#D0BFFF] px-2 py-2 rounded-lg"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                class="w-6 h-6"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3.75 12a.75.75 0 01.75-.75h15a.75.75 0 010 1.5h-15a.75.75 0 01-.75-.75z"
-                  clip-rule="evenodd"
-                />
-              </svg>
+              <FaArrowDown/>
             </button>
+            </div>
           </div>
-          <div className="flex items-center">
-            {/* Add up and down arrow buttons */}
+
+          {/* @dev section for minutes */}
+          <div className="flex flex-col items-center gap-2">
+            <span className="font-bold">Minutes</span>
+            <div className="flex flex-col items-center gap-2">
             <button
               onClick={() =>
                 setStreakGoal({
@@ -90,31 +73,21 @@ function AskGoal({ handleGoalSubmit }) {
                   minutes: (parseInt(streakGoal.minutes, 10) + 1).toString(),
                 })
               }
-              className="bg-[#D0BFFF] px-2 py-2 rounded-full mr-2"
+              className="bg-[#D0BFFF] px-2 py-2 rounded-lg"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <FaArrowUp/>
             </button>
+            {/* get the minutes */}
             <input
-              type="text"
+              type="number"
               onChange={(e) =>
                 setStreakGoal({ ...streakGoal, minutes: e.target.value })
               }
               value={streakGoal.minutes}
-              className="w-20 border border-[#BEADFA] p-3 rounded-lg outline-none mr-2"
+              className="w-20 border border-[#BEADFA] p-3 rounded-lg text-center"
+              aria-label="Minutes"
             />
-            <span>minutes</span>
-            {/* Add up and down arrow buttons */}
+
             <button
               onClick={() =>
                 setStreakGoal({
@@ -125,34 +98,26 @@ function AskGoal({ handleGoalSubmit }) {
                   ).toString(),
                 })
               }
-              className="bg-[#D0BFFF] px-2 py-2 rounded-full ml-2"
+              className="bg-[#D0BFFF] px-2 py-2 rounded-lg"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                class="w-6 h-6"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3.75 12a.75.75 0 01.75-.75h15a.75.75 0 010 1.5h-15a.75.75 0 01-.75-.75z"
-                  clip-rule="evenodd"
-                />
-              </svg>
+              <FaArrowDown/>
             </button>
+            </div>
           </div>
         </div>
-        <button
-          onClick={() => {
-            handleGoalSubmit();
-            updateLocalStorage();
-          }}
-          className="mx-auto mt-4 px-6 py-3 bg-[#D0BFFF] hover:bg-[#BEADFA] rounded-full font-bold"
-        >
-          Submit
-        </button>
-        <p className="mt-4 text-center">
-          This will determine your streakGoal, so set accordingly!
+        <div className="flex justify-center mt-6">
+          <button
+            onClick={() => {
+              handleGoalSubmit();
+              updateLocalStorage();
+            }}
+            className="px-6 py-3 bg-indigo-500 hover:bg-indigo-600 text-white rounded-full font-semibold"
+          >
+            Submit
+          </button>
+        </div>
+        <p className="mt-4 text-center text-gray-600 italic">
+          This will determine your study streak goal, so set it accordingly!
         </p>
       </div>
     </div>
