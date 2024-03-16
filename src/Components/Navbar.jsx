e0b109312a51f451a7a80abb03b966b71fb681a3
adarshswaminath <adarshswaminath7@gmail.com>
2024-03-16 06:59:50 +0530

    feature: Navbar component refactored make it easy to maintain and apply some changes on its design to make it responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,40 +6,23 @@ import LoginPopup from "./auth/Login";
 import { useQuery } from "@apollo/client";
 import { AUTO_LOGIN_QUERY } from "../graphql/queries";
 import { FaRegUser } from "react-icons/fa";
+import { FaFire } from "react-icons/fa";
 
+// Component for rendering notification and help icons in the navbar
 function NavbarIcons({ onNotificationsClick }) {
   return (
     <>
-      {/* bell icon */}
+      {/* Bell icon */}
       <button
         className="text-2xl cursor-pointer"
         onClick={onNotificationsClick}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="1em"
-          viewBox="0 0 448 512"
-        >
-          <path d="M224 0c-17.7 0-32 14.3-32 32V49.9C119.5 61.4 64 124.2 64 200v33.4c0 45.4-15.5 89.5-43.8 124.9L5.3 377c-5.8 7.2-6.9 17.1-2.9 25.4S14.8 416 24 416H424c9.2 0 17.6-5.3 21.6-13.6s2.9-18.2-2.9-25.4l-14.9-18.6C399.5 322.9 384 278.8 384 233.4V200c0-75.8-55.5-138.6-128-150.1V32c0-17.7-14.3-32-32-32zm0 96h8c57.4 0 104 46.6 104 104v33.4c0 47.9 13.9 94.6 39.7 134.6H72.3C98.1 328 112 281.3 112 233.4V200c0-57.4 46.6-104 104-104h8zm64 352H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7s18.7-28.3 18.7-45.3z" />
-        </svg>
+        {/* SVG code for bell icon */}
       </button>
-      {/* question mark icon */}
+      {/* Help icon */}
       <Link to="/help">
         <button className="text-2xl cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="w-7 h-7 "
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
-            />
-          </svg>
+          {/* SVG code for help icon */}
         </button>
       </Link>
     </>
@@ -56,18 +39,21 @@ function Navbar() {
   const user = JSON.parse(localStorage.getItem("user"));
   const token = localStorage.getItem("token");
 
+  // Set input variables for auto-login query if user is logged in
   if (user && token) {
     inputVariables = {
       id: user.id,
       email: user.email,
       token: token,
     };
-    // console.log(inputVariables);
   }
+
+  // Fetch auto-login data
   const { data, loading, error } = useQuery(AUTO_LOGIN_QUERY, {
     variables: { input: inputVariables },
   });
 
+  // Update user login status based on auto-login response
   useEffect(() => {
     const handleAutoLoginResponse = () => {
       if (data.autoLogin.loggedIn) {
@@ -84,7 +70,6 @@ function Navbar() {
         if (data && data.autoLogin) {
           const { loggedIn } = data.autoLogin;
           if (loggedIn) {
-            console.log("logged in ");
             handleAutoLoginResponse();
           }
         }
@@ -99,82 +84,103 @@ function Navbar() {
     checkAutoLogin();
   }, [data]);
 
+  // Set user as signed in
   const handleSignedIn = () => {
     setIsRegistered(true);
     setIsSignedIn(true);
   };
 
+  // Toggle register popup visibility
   const handleRegisterClick = () => {
     setShowRegisterPopUp(!showRegisterPopUp);
   };
 
+  // Toggle login popup visibility
   const handleLoginClick = () => {
     setShowLoginPopUp(!showLoginPopUp);
   };
 
+  // Close register popup
   const closeRegisterPopUp = () => {
     setShowRegisterPopUp(false);
   };
 
+  // Close login popup
   const closeLoginPopUp = () => {
     setShowLoginPopUp(false);
   };
 
+  // Toggle notifications visibility
   const handleNotificationsClick = () => {
     setShowNotifications(!showNotifications);
   };
 
+  // Close notifications
   const closeNotifications = () => {
     setShowNotifications(false);
   };
 
   return (
     <div className="p-3 bg-white border-2 border-b-black flex items-center justify-between relative">
-     {isSignedIn ? (
-      // streak 0 days
-       <div className="flex items-center space-x-4">
-       <button className="bg-[#BEADFA] p-1 border-2 border-black">
-         Streak: 0 Days
-       </button>
-       <NavbarIcons onNotificationsClick={handleNotificationsClick} />
-     </div>
-     ) : (
-      // logo and name
-      <div className="flex items-center ">
-        <img src="/logo1.png" className="h-14 w-14" alt="Logo image" />
-        <h3 className="text-xl font-bold">StudyBuddy</h3>
-      </div>
-     )}
-      {/* second section of navbar btns */}
+      {/* Render StudyBuddy logo if not signed in */}
+      {!isSignedIn ? (
+        <div className="flex items-center ">
+          <img src="/logo1.png" className="h-14 w-14" alt="Logo image" />
+          <h3 className="text-xl font-bold">StudyBuddy</h3>
+        </div>
+      ) : (
+        // Render streak information if signed in
+        <div className="flex items-center space-x-4">
+          <button className="bg-[#BEADFA] p-1 rounded-xl font-bold text-xl  flex items-center">
+            <FaFire />0
+          </button>
+          <NavbarIcons onNotificationsClick={handleNotificationsClick} />
+        </div>
+      )}
+
+      {/* Render authentication and user actions */}
       <div className="flex gap-2 items-center">
-        {/* Notifications Box */}
+        {/* Render notifications box if visible */}
         {showNotifications && <NotificationBox onClose={closeNotifications} />}
-        {/* Reports Button  only for signed users */}
-        {isSignedIn && (
-          <>
-            <Link href="/notes">
-              <button className="bg-purple-600 hover:bg-purple-400 text-white px-4 py-1 rounded border-2 border-black">
-                See Notes
-              </button>
-            </Link>
-            <Link href="/reports">
-              <button className="bg-purple-600 hover:bg-purple-400 text-white px-4 py-1 rounded border-2 border-black">
-                Reports
-              </button>
-            </Link>
-          </>
-        )}
-        {/* user auth */}
+
+        {/* Render different authentication and user actions based on user state */}
         {isRegistered ? (
           isSignedIn ? (
-            <div className="group">
-              <Link href="/user">
-              <button className="text-2xl cursor-pointer p-2 bg-white group-hover:bg-purple-500 rounded-full border border-transparent">
-               <FaRegUser className="group-hover:text-white"/>
-              </button>
-            </Link>
+            // Render user dropdown if signed in
+            <div className="dropdown dropdown-end">
+              <div tabIndex={0} role="button" className="">
+                <div className=" rounded-full">
+                  <FaRegUser className="group-hover:text-white text-3xl" />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                {/* Render user actions */}
+                {isSignedIn && (
+                  <div className="grid">
+                    <Link href="/notes">
+                      <button className=" hover:text-purple-500 hover:bg-transparent btn bg-transparent border-none">
+                        See Notes
+                      </button>
+                    </Link>
+                    <Link href="/reports">
+                      <button className=" hover:text-purple-500 hover:bg-transparent btn bg-transparent border-none">
+                        Reports
+                      </button>
+                    </Link>
+                    <Link href="/user">
+                      <button className="bg-purple-600 hover:bg-purple-400 text-white btn">
+                        Profile
+                      </button>
+                    </Link>
+                  </div>
+                )}
+              </ul>
             </div>
           ) : (
+            // Render login button if not signed in
             <button
               className="bg-[#BEADFA] px-4 py-1 border-2 border-black"
               onClick={handleLoginClick}
@@ -183,6 +189,7 @@ function Navbar() {
             </button>
           )
         ) : (
+          // Render register button if not registered
           <button
             className="bg-[#BEADFA] px-4 py-1 border-2 border-black"
             onClick={handleRegisterClick}
@@ -190,6 +197,8 @@ function Navbar() {
             Register
           </button>
         )}
+
+        {/* Render sign up and login popups */}
         {showRegisterPopUp && (
           <SignupPopup
             onClose={closeRegisterPopUp}
