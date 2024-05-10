56ce6b5f4e1d72e4f52abfd61e979d88eec5bd18
unknown <saharialeora2406@gmail.com>
2024-05-10 17:30:46 +0530

    color scheme

--- src/Components/SessionComponents/MusicControls.jsx
+++ src/Components/SessionComponents/MusicControls.jsx
@@ -1,12 +1,11 @@
-import { React, useState, useEffect } from "react";
-import { useLocation } from "wouter";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faVolumeMute,
-  faVolumeHigh,
-  faVolumeLow,
   faMusic,
+  faVolumeHigh,
+  faVolumeMute
 } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { React, useState } from "react";
+import { useLocation } from "wouter";
 
 const MusicControls = () => {
   // music controls logic
